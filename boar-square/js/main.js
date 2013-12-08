8a120666f79a3a3ba91eb61e5596bd3003908ce9
Henry Van Dusen <hvandusen@dyn-160-39-229-63.dyn.columbia.edu>
2013-12-08 16:37:59 -0500

    added location not found alert thing

--- boar-square/js/main.js
+++ boar-square/js/main.js
@@ -620,9 +620,13 @@ function getRandomColor()  {
     return color;
 }
 
+console.log($.support.cors);
+
 function queryFoursquare(queryString, sectionName) {
+    $('#notFound').css('visibility','hidden');
+
     var lat,lon;
-    $.getJSON(queryString, function( data ) {
+    var search = $.getJSON(queryString, function( data ) {
        // console.log('objects from section ' + sectionName);
         var thisCategory = foursquareSectionToCat[sectionName];
         nearbyVenues[thisCategory] = toNearbyVenues(data.response.groups[0].items, sectionName); //all the nearby places
@@ -638,9 +642,6 @@ function queryFoursquare(queryString, sectionName) {
         }
         avLat = totalLatitudes / theseVenues.length;
         avLon = totalLongitudes / theseVenues.length;
-        console.log("la: "+avLat+"lo: "+avLon);
-
-
         //console.dir(theseVenues);
 
         if(resetMap)
@@ -680,10 +681,14 @@ function queryFoursquare(queryString, sectionName) {
             addToEnvironment(thisCategory, v, selected);
         }
         searchVenuesCounter++;
-    },'text');
+
+    }).fail(function(){
+        $('#notFound').css('visibility','visible');
+    });
 }
 
 function querySpecificVenueFoursquare(venueTerms, location, categoryName) {
+    $('#notFound').css('visibility','hidden');  //remove error message
     var queryString = 'https://api.foursquare.com/v2/venues/search?' +
         'query=' + venueTerms + 
         '&near=' + location + 
@@ -697,10 +702,12 @@ function querySpecificVenueFoursquare(venueTerms, location, categoryName) {
         var categoryId = categoryIds[categoryName];
         queryString += '&categoryId=' + categoryId;
     }
-    $.getJSON(queryString, function(data) {
+    var search = $.getJSON(queryString, function(data) {
         //console.log(venueTerms + ' results:');
 
         var bestMatch = data.response.venues[0];
+        if(bestMatch == null)
+            $('#notFound').css('visibility','visible');
         //console.log(bestMatch.id);
         var photoQuery = 'https://api.foursquare.com/v2/venues/' + bestMatch.id + '/photos?' +
             'limit=1' + 
@@ -714,7 +721,9 @@ function querySpecificVenueFoursquare(venueTerms, location, categoryName) {
             addedPlace = addToEnvironment("Restaurant", niceMatch, true);
             addNewLocation("Restaurant", addedPlace, true);
         })        
-    }, 'text');
+    }).fail(function(){
+        $('#notFound').css('visibility','visible');
+    });
 }
 
 // get the parameters in our url. Sourced from stackoverflow (sort of).
